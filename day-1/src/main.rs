d0f042df5f5f864626a3702ef52edde999ef9447
Lee Jonghyuk <jhlee@superb-ai.com>
2023-01-23 19:07:36 -0800

    Day 1

---
+++ day-1/src/main.rs
@@ -0,0 +1,33 @@
+use std::cmp::Reverse;
+use std::fs;
+
+fn get_calories(filename: &str) -> Vec<i32> {
+    fs::read_to_string(filename)
+        .expect("Input for the first star")
+        .split("\n\n")
+        .map(|items| {
+            items
+                .split("\n")
+                .map(|item| item.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect()
+}
+
+fn part1() {
+    let calories: Vec<i32> = get_calories("input1.txt");
+    let res = calories.iter().max().unwrap();
+    println!("{}", res);
+}
+
+fn part2() {
+    let mut calories: Vec<i32> = get_calories("input2.txt");
+    calories.sort_by_key(|v| Reverse(*v));
+    let res = calories.iter().take(3).sum::<i32>();
+    println!("{}", res);
+}
+
+fn main() {
+    part1();
+    part2();
+}
